434657fd5a2968eeb9185afc8bb52f94999f6bdb
vega <a@v.com>
2025-11-13 20:45:11 -0500

    vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['purrsephone.local.vega.nyc'],
     proxy: {
       '/api': {
         target: 'http://localhost:3456',
